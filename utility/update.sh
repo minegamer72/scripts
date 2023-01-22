9d01478705a2d2d8dd039549b77ed51ac27f57f6
bob <108271839+minegamer72@users.noreply.github.com>
2023-01-22 07:58:44 -0500

    make code easier to read

--- utility/update.sh
+++ utility/update.sh
@@ -3,77 +3,68 @@
 # I use this for updating all of my packages on my arch *snort* install. #
 # Requirements: flatpak, yay, pacman #
 #
-# For people who use a distro with apt: You will need to remove the entire if statement on line 38 > 48 and replace
-# the if statement on line 27 > 36 to be apt update && apt upgrade or whatever update command it is now. #
+# For people who use a distro with apt: You will need to remove the entire if statement on line 34 > 43 and replace
+# the if statement on line 23 > 32 to be apt update && apt upgrade or whatever update command it is now. #
 
-# check if running as root #
-if [ "$EUID" == 0 ]
-  then echo "DO NOT RUN THIS WITH ROOT UNLESS YOU ABSOLUTELY NEED TO!! "
-echo
+# Check if running as root
+if [ "$EUID" == 0 ]; then
+  echo "DO NOT RUN THIS WITH ROOT UNLESS YOU ABSOLUTELY NEED TO!"
+  echo
 
-# prompt for continuing anyway #
-
-read -p "CONTINUE ANYWAY?" -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-echo
-exit
-fi
+  # Prompt for continuing anyway
+  read -p "CONTINUE ANYWAY? (Y/N) " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo
+    exit
+  fi
 fi
 echo
 
-# prompt pacman update #
-
-read -p "update pacman packages? Y or N (case insensitive) " -n 1 -r
+# Prompt for updating pacman packages
+read -p "Update pacman packages? (Y/N) " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-echo "skipping pacman -Syu"
-echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Skipping pacman -Syu"
+  echo
 else
-sudo pacman -Syu -y
-echo
+  sudo pacman -Syu -y
+  echo
 fi
 
-# prompt update yay packages #
-
-read -p "update yay packages? Y or N (case insensitive) " -n 1 -r
+# Prompt for updating yay packages
+read -p "Update yay packages? (Y/N) " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then echo "skipping yay -Syu"
-echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Skipping yay -Syu"
+  echo
 else
-yay -Syu -y
-echo
+  yay -Syu -y
+  echo
 fi
 
-# prompt update flatpak packages #
-
-if [ "$EUID" == 0 ] # check if root #
-then
-echo "skipping flatpak update due to script being ran as root" # stop the script from updating flatpak packages as root #
-echo
+# Prompt for updating flatpak packages
+if [ "$EUID" == 0 ]; then
+  echo "Skipping flatpak update due to script being ran as root"
+  echo
 else
+  read -p "Update flatpak packages? (Y/N) " -n 1 -r
+  echo
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Skipping command flatpak update"
+    echo
+  else
+    flatpak update
+    echo
+  fi
+fi
 
-read -p "update flatpak packages? Y or N (case insensitive) " -n 1 -r
+# Prompt for rebooting
+read -p "Reboot? (Y/N) " -n 1 -r
 echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then echo "skipping command flatpak update"
-echo
-
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "Not rebooting"
+  echo
+  exit
 else
-flatpak update
-echo
+  reboot
 fi
-fi
-
-read -p "reboot? Y or N (case insensitive) " -n 1 -r
-echo
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then echo "not rebooting"
-echo
-exit
-else
-reboot
-fi
-
